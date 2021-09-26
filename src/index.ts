267bd4a38a487c9c68f7faf030601a50d4e2c29b
Guillermo Garcia Soto <gasogui@gmail.com>
2021-09-25 20:46:15 -0500

    Full server configuration

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
+/* ---  index.ts  --- */
 import  express, {Application}  from "express";
+import morgan from 'morgan';
+import cors from 'cors';
 
 import indexRoutes from './routes/indexRoutes';
 import movieRoutes from './routes/moviesRoutes';
@@ -15,6 +18,11 @@ class Server{
 
   config(): void {
     this.app.set('port', process.env.PORT || 3000);
+    this.app.use(morgan('dev'));
+    this.app.use(cors());
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({extended: false}));
+
   }
 
   routes(): void {
